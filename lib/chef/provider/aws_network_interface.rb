89de24d0b572cbd916057d6313e01ba0dd8f8a2a
Patrick Wright <patrick@getchef.com>
2015-02-09 13:49:01 -0700

    aws_network_interface scaffolding

---
+++ lib/chef/provider/aws_network_interface.rb
@@ -0,0 +1,15 @@
+require 'chef/provider/aws_provider'
+
+class Chef::Provider::AwsNetworkInterface < Chef::Provider::AwsProvider
+  action :create do
+  end
+
+  action :delete do
+  end
+
+  action :attach do
+  end
+
+  action :detach do
+  end
+end
